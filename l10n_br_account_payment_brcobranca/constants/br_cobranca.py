127b53df1406159a271bfb003d94d58c5562fff7
root <root@dev.rivierasl.com.br>
2021-11-24 02:12:16 -0300

    Corrigindo para n~ao salvar no conf

--- l10n_br_account_payment_brcobranca/constants/br_cobranca.py
+++ l10n_br_account_payment_brcobranca/constants/br_cobranca.py
@@ -6,7 +6,6 @@
 
 import os
 from collections import namedtuple
-
 from odoo import _
 from odoo.exceptions import Warning as UserError
 
@@ -56,7 +55,8 @@ def get_brcobranca_bank(bank_account_id, payment_method_code):
 
 
 def get_brcobranca_api_url():
-    brcobranca_api_url = os.environ.get("BRCOBRANCA_API_URL")
+    brcobranca_api_url = self.env['ir.config_parameter'].get_param('BRCOBRANCA_API_URL')
+#     brcobranca_api_url = os.environ.get("BRCOBRANCA_API_URL")
 
     if not brcobranca_api_url:
         raise UserError(
